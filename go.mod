8a050698d6aabed6018c5f1c7edcea7b359eb01f
minh352623 <congminh352623@gmail.com>
2025-01-14 10:42:28 +0700

    ✨ feat: Logs and variable

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module hello
 
 go 1.23.4
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rs/zerolog v1.33.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/rs/zerolog/pkgerrors"
+)
+
 
 func main() {
-	fmt.Println("Hello, World!")
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	log.Debug().Msg("Hello, World!")
+	 log.Log().
+        Str("foo", "bar").
+        Msg("")
+
+	err := errors.New("seems we have an error here")
+	log.Error().Err(err).Msg("oh no")
+
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
+
+	err = outer()
+	log.Error().Stack().Err(err).Msg("")
+
+	var a int = 1
+	var b int = 2
+	b, a = a, b
+	log.Error().Int("a", a).Int("b", b).Msg("")
+}
+
+func inner() error {
+	return errors.New("seems we have an error here")
+}
+
+func middle() error {
+	err := inner()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func outer() error {
+	err := middle()
+	if err != nil {
+		return err
+	}
+	return nil
 }
